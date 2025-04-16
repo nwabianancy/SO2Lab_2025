62cdccf209e08aa861fdd6679dc4562cd77c9c34
Purple_Girl <88599342+nwabianancy@users.noreply.github.com>
2025-04-16 18:32:54 +0200

    Update Head.java

--- Head.java
+++ Head.java
@@ -6,7 +6,8 @@ import java.awt.Graphics;
 public class Head {
 	private int width, height;
 	private int x, y;
-	
+	private Eye eye;
+	private mouth mouth;
 	
 	 public Head(int x, int y, int width, int height) {
 			this.x = x;
@@ -25,6 +26,8 @@ public class Head {
 	 public void draw(Graphics pen) {
 		 pen.setColor(Color.decode("#f7abe9"));
 		 pen.fillOval(x, y, width, height);
+		 mouth.drawAt();
+		 eye.drawAt();
 		
 	 }
 	   }
