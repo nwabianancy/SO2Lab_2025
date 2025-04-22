69c0cc7e0fe0f9862978bf56c62ebce5e79e9e3c
Purple_Girl <88599342+nwabianancy@users.noreply.github.com>
2025-04-23 00:16:58 +0200

    Update Dragonfly.java

--- Dragonfly.java
+++ Dragonfly.java
@@ -1,16 +1,28 @@
 package drawingTool;
 
+import java.awt.Color;
 import java.awt.Graphics;
 
 public class Dragonfly {
 	private Head head;
 	private Thorax thorax;
 	//private Mouth mouth;
+	private Abdomen abdomen;
+	private Wing leftWing1;
+	private Wing leftWing2;
+	private Wing rightWing1;
+	private Wing rightWing2;
 	
-	public Dragonfly(int x, int y,int width, int height) {
+	public Dragonfly(int width, int height) {
 		
-		 head= new Head(x,y, width, height);
-		 thorax= new Thorax(x,y, width, height);
+		 head = new Head(100);
+		 thorax = new Thorax(head.getSize());
+		 abdomen = new Abdomen(head.getSize());
+		 leftWing1 = new Wing(head.getSize());
+		 leftWing2 = new Wing(head.getSize());
+		 rightWing1 = new Wing(head.getSize());
+		 rightWing2 = new Wing(head.getSize());
+			
 	}
 	
 	/*
@@ -21,8 +33,17 @@ public class Dragonfly {
 	 */
 
 		public void draw(Graphics pen) {
-			head.draw(pen);
-			thorax.draw(pen);
+			rightWing1.drawAt(700, 300);
+			rightWing2.drawAt(700, 330);
+			leftWing1.drawAt(900, 300);
+			leftWing2.drawAt(900, 330);
+			abdomen.drawAt(800,280);
+			head.drawAt(800,170);
+			thorax.drawAt(800,250);
+			
+			
+			
+			
 			
 		}
 
