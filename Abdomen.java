c6aaad30e39e285aff2cf5f831d0bdfc51ccebb7
Purple_Girl <88599342+nwabianancy@users.noreply.github.com>
2025-04-23 00:19:30 +0200

    Update Abdomen.java

--- Abdomen.java
+++ Abdomen.java
@@ -1,5 +1,24 @@
 package drawingTool;
 
-public class Abdomen {
+import java.awt.Color;
 
+public class Abdomen {
+	private int height;
+	private int width;
+	
+	public Abdomen(int headSize) {
+	this.height = headSize * 2; 
+	this.width = (int)(headSize * 0.5);     
+	}
+ public void drawAt(int left, int bottom) {
+	int halfWidth = width/2;
+    int halfHeight = height/2;
+    Drawing.pen.setColor(Color.decode("#ff88d5"));
+	Drawing.pen.fillOval(left - halfWidth, bottom + halfHeight, width, height);
+	 // Draw black outline
+    Drawing.pen.setColor(Color.BLACK);
+    Drawing.pen.drawOval(left - halfWidth, bottom + halfHeight, width, height);
+	
+ 	}
+ 
 }
