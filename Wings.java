4e70b47c5d3ef1d8ef95f9fad7b88edb58a38c9f
Purple_Girl <88599342+nwabianancy@users.noreply.github.com>
2025-04-23 00:18:37 +0200

    Update Wings.java

--- Wings.java
+++ Wings.java
@@ -1,5 +1,24 @@
 package drawingTool;
 
-public class Wings {
+import java.awt.Color;
 
+public class Wing {
+	private int height;
+	private int width;
+	
+	public Wing(int headSize) {
+	this.height = (int)(headSize*0.3); 
+	this.width = (int)(headSize * 2);     
+	}
+ public void drawAt(int left, int bottom) {
+	int halfWidth = width/2;
+    int halfHeight = height/2;
+    Drawing.pen.setColor(Color.decode("#55e1ff"));
+	Drawing.pen.fillOval(left - halfWidth, bottom + halfHeight, width, height);
+	 // Draw black outline
+    Drawing.pen.setColor(Color.BLACK);
+    Drawing.pen.drawOval(left - halfWidth, bottom + halfHeight, width, height);
+	
+ 	}
+ 
 }
