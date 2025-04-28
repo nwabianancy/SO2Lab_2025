fea94f375ed9b132545b2b6e9696e00e7976c498
Purple_Girl <88599342+nwabianancy@users.noreply.github.com>
2025-04-28 09:43:13 +0200

    Update Dragonfly.java

--- Dragonfly.java
+++ Dragonfly.java
@@ -1,47 +1,33 @@
 package drawingTool;
 
-import java.awt.Color;
-import java.awt.Graphics;
+/*import java.awt.Color;
+import java.awt.Graphics;*/
 
 public class Dragonfly {
 	private Head head;
 	private Thorax thorax;
-	//private Mouth mouth;
 	private Abdomen abdomen;
-	private Wing leftWing1;
-	private Wing leftWing2;
-	private Wing rightWing1;
-	private Wing rightWing2;
 	
-	public Dragonfly(int width, int height) {
-		
-		 head = new Head(100);
+	private int baseSize;
+	
+	public Dragonfly(int baseSize) {
+		this.baseSize = baseSize;
+		 head = new Head(baseSize);
 		 thorax = new Thorax(head.getSize());
 		 abdomen = new Abdomen(head.getSize());
-		 leftWing1 = new Wing(head.getSize());
-		 leftWing2 = new Wing(head.getSize());
-		 rightWing1 = new Wing(head.getSize());
-		 rightWing2 = new Wing(head.getSize());
 			
 	}
-	
-	/*
-	 * public void drawAt(int left, int bottom) { head.drawAt(left, bottom);
-	 * 
-	 * 
-	 * }
-	 */
 
-		public void draw(Graphics pen) {
-			rightWing1.drawAt(700, 300);
-			rightWing2.drawAt(700, 330);
-			leftWing1.drawAt(900, 300);
-			leftWing2.drawAt(900, 330);
-			abdomen.drawAt(800,280);
-			head.drawAt(800,170);
-			thorax.drawAt(800,250);
-			
-			
+		public void draw(int left, int bottom) {
+			//I feel these are magic numbers and that it would be better to use division and multiplication with whole integers or floating numbers
+			/*
+			 * rightWing1.drawAt(left, bottom-270); rightWing2.drawAt(left, bottom-300);
+			 * leftWing1.drawAt(left-200, bottom-270); leftWing2.drawAt(left-200,
+			 * bottom-300);
+			 */
+			abdomen.drawAt(left +2 , bottom - baseSize * 3);
+			head.drawAt(left-13, bottom - baseSize*5 );//450
+			thorax.drawAt(left -4, bottom- baseSize * 4);	
 			
 			
 			
