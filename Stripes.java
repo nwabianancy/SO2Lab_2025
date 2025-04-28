8ca140272ac2d486636dc80fede683e58ee6ddd3
Purple_Girl <88599342+nwabianancy@users.noreply.github.com>
2025-04-28 09:52:29 +0200

    Update and rename Strips.java to Stripes.java

---
+++ Stripes.java
@@ -0,0 +1,16 @@
+package drawingTool;
+
+public class Stripes {
+	private int size;
+	
+	
+	public Stripes() {
+		
+	}
+	
+	public void drawAt (int left, int bottom) {
+		
+		
+	}
+
+}
--- Strips.java
+++
@@ -1,5 +0,0 @@
-package drawingTool;
-
-public class Strips {
-
-}
