d8c41cfea3418f6a0791a9665f69a29ba102c19f
Purple_Girl <88599342+nwabianancy@users.noreply.github.com>
2025-04-28 09:49:06 +0200

    Update Eye.java

--- Eye.java
+++ Eye.java
@@ -1,5 +1,21 @@
 package drawingTool;
 
 public class Eye {
+	private int width;
+    private int height;
+    private int startAngle;
+    private int arcAngle;
+	
+	public Eye(int width, int height, int startAngle, int arcAngle){
+		this.width = width;
+        this.height = height;
+        this.startAngle = startAngle;
+        this.arcAngle = arcAngle;
+	}
+
+	public void drawAt(int left, int bottom) {
+		Drawing.pen.drawArc(left, bottom, width, height, startAngle, arcAngle);
+		
+	}
 
 }
