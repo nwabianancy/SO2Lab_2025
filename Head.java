12939d389b6adcaf69b3efe277ba3d1bf4cf82cc
Purple_Girl <88599342+nwabianancy@users.noreply.github.com>
2025-04-28 09:50:12 +0200

    Update Head.java

--- Head.java
+++ Head.java
@@ -1,25 +1,45 @@
 package drawingTool;
 
 import java.awt.Color;
-import java.awt.Graphics;
+/*import java.awt.Graphics;*/
 
 public class Head {
     private int size;
+    private Eye leftEye;
+    private Eye rightEye;
+    private Mouth mouth;
   
-    public Head(int size) {
-        this.size = size;
+    public Head(int baseSize) {
+        this.size = baseSize;
+        
+        int eyeWidth = size/4;
+        int eyeHeight = size/5;
+        int eyeArcStart = 0;
+        int eyeArcAngle = 180;
+
+        leftEye = new Eye(eyeWidth, eyeHeight, eyeArcStart, eyeArcAngle);
+        rightEye = new Eye(eyeWidth, eyeHeight, eyeArcStart, eyeArcAngle);
+
+        int mouthWidth = size/2;
+        int mouthHeight = size/4;
+        int mouthStart = 0;
+        int mouthAngle = 180;
+
+        mouth = new Mouth(mouthWidth, mouthHeight, mouthStart, mouthAngle);
     }
 
-    public void drawAt(int left, int bottom) {
-        int halfTheSize = size/2;
-        int x = left - halfTheSize; 
-        int y = bottom + halfTheSize;
+    
+
+	public void drawAt(int left, int bottom) {
+        
+        
         Drawing.pen.setColor(Color.decode("#ff88d5"));
-        Drawing.pen.fillOval(x, y, size, size); 
+        Drawing.pen.fillOval(left, bottom, size, size); 
         Drawing.pen.setColor(Color.black);
-        Drawing.pen.drawOval(x, y, size, size);
-		//mouth.drawAt();
-		//eye.drawAt();
+        Drawing.pen.drawOval(left, bottom, size, size);
+        
+        //mouth.drawAt(left, bottom ); //- 10 bottom - 500
+		
     }
 
   
