b59379c21c29f7f35346a70d84a9a2d626219274
Purple_Girl <88599342+nwabianancy@users.noreply.github.com>
2025-04-28 09:51:35 +0200

    Update Mouth.java

--- Mouth.java
+++ Mouth.java
@@ -1,5 +1,21 @@
 package drawingTool;
 
-public class Mouth {
+import java.awt.Color;
 
+public class Mouth {
+	private int width;
+    private int height;
+    private int startAngle;
+    private int arcAngle;
+
+    public Mouth(int width, int height, int startAngle, int arcAngle) {
+        this.width = width;
+        this.height = height;
+        this.startAngle = startAngle;
+        this.arcAngle = arcAngle;
+       
+	}
+	public void drawAt(int left, int bottom) {
+		Drawing.pen.drawArc(left, bottom, width, height, startAngle, arcAngle);
+	}
 }
