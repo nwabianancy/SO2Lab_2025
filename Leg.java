f4d984dc5d1d3fb24ac7f5d3b339a3abc5780a92
Purple_Girl <88599342+nwabianancy@users.noreply.github.com>
2025-04-28 09:50:42 +0200

    Update Leg.java

--- Leg.java
+++ Leg.java
@@ -1,5 +1,18 @@
 package drawingTool;
 
 public class Leg {
-
+	private int height;
+	private int width;
+	private int startAngle;
+    private int arcAngle;
+	
+	public Leg(int width, int height, int startAngle, int arcAngle) {
+		this.height= height;
+		this.width = width;
+		this.startAngle = startAngle;
+        this.arcAngle = arcAngle;
+	}
+	public void drawAt(int left, int bottom) {
+		Drawing.pen.drawArc(left, bottom, width, height, startAngle, arcAngle);
+	}
 }
