6cef04003c51e3466bf6eb58816077844465c293
Purple_Girl <88599342+nwabianancy@users.noreply.github.com>
2025-04-28 09:38:30 +0200

    Update Wings.java

--- Wings.java
+++ Wings.java
@@ -5,20 +5,20 @@ import java.awt.Color;
 public class Wing {
 	private int height;
 	private int width;
+	private Stripes wingStripe;
 	
-	public Wing(int headSize) {
-	this.height = (int)(headSize*0.3); 
-	this.width = (int)(headSize * 2);     
+	public Wing(int height, int width) {
+		this.height = height;
+		this.width = width;
+		wingStripe = new Stripes();  
+	
 	}
  public void drawAt(int left, int bottom) {
-	int halfWidth = width/2;
-    int halfHeight = height/2;
     Drawing.pen.setColor(Color.decode("#55e1ff"));
-	Drawing.pen.fillOval(left - halfWidth, bottom + halfHeight, width, height);
-	 // Draw black outline
+	Drawing.pen.fillOval(left , bottom , width, height);
     Drawing.pen.setColor(Color.BLACK);
-    Drawing.pen.drawOval(left - halfWidth, bottom + halfHeight, width, height);
-	
+    Drawing.pen.drawOval(left , bottom, width, height);
+    
  	}
  
 }
